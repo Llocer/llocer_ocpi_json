e4a34ae868bfeb1761014f76fe921bda4b116f69
pma <pma@llocer.net>
2021-11-18 11:10:32 +0100

    +

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,5 @@
 module com.llocer.ev.msgs.ocpi {
-	exports com.llocer.ev.msgs.ocpi.Common;
+	exports com.llocer.ev.msgs.ocpi;
 
 	requires com.fasterxml.jackson.annotation;
 	requires java.compiler;
